81fa85ba2b662a9b5b9852b70e3d572f8ff778d0
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2025-05-15 12:20:30 +0530

    now index.html can gen autometically after app build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: {
@@ -16,4 +18,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      //   template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+  ],
 };
